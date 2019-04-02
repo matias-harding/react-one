be061e5b6457bc99a5d14456edc0d44da707729c
Matias Harding <mharding@ecomsur.com>
2019-04-01 23:53:33 -0300

    State: class state and persons array

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 25},
+      {name: 'Matt', age: 36},
+      {name: 'Alejandro', age: 44}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello</h1>
         <p>This is a paragraph</p>
-        <Person name="Max" age="25" />
-        <Person name="Matt" age="36" > My hobies: Aviation</Person>
-        <Person name="Alejandro" age="44" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My hobies: Aviation</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'I\'m a title'));
