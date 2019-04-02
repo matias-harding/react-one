4d7a56db514e9432a20ca048dbad7fe7714cb4b1
Matias Harding <mharding@ecomsur.com>
2019-04-01 23:11:06 -0300

    Person

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Start</h1>
+        <h1>Hello</h1>
+        <p>This is a paragraph</p>
+        <Person name="Max" age="25" />
+        <Person name="Matt" age="36" > My hobies: Aviation</Person>
+        <Person name="Alejandro" age="44" />
       </div>
     );
+    // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'I\'m a title'));
   }
 }
 
