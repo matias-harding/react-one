ad40852d0dd9989598bb9b52e4fb3cff10f358e2
Matías Harding <mharding@ecomsur.com>
2019-05-28 17:36:51 -0400

    .bind(this, "algo" ): onClick and click examples

--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log("Was clicked!");
     // NOT's: this.state.person[0].name = 'Lolo Palanca';
     this.setState({
       persons: [
-        {name: 'Maximilian', age: 25},
+        {name: newName, age: 25},
         {name: 'Matt', age: 36},
         {name: 'Alejandro', age: 21}
       ]
@@ -29,10 +29,17 @@ class App extends Component {
       <div className="App">
         <h1>Hello</h1>
         <p>This is a paragraph</p>
-        <button onClick={this.switchNameHandler}>Switch Me!!</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My hobies: Aviation</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Me!!</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Matts!!')} > My hobbies: Aviation</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div',{className: 'App'},React.createElement('h1',null,'I\'m a title'));
