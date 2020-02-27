be767a4801c74c29aa32c3c245041146493f8343
Matias Harding <matias.harding@gmail.com>
2020-02-26 21:59:17 -0300

    styled component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: green;
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: lightgreen;
+    color: black
+  }
+`;
 
 class App extends Component {
   state = {
@@ -42,18 +56,6 @@ class App extends Component {
   }
 
   render () {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
 
     let persons = null;
 
@@ -71,11 +73,11 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      };
+      // style.backgroundColor = 'red';
+      // style[':hover'] = {
+      //   backgroundColor: 'salmon',
+      //   color: 'black'
+      // };
     }
 
     const classes = [];
@@ -90,9 +92,10 @@ class App extends Component {
         <div className="App">
           <h1>Su app</h1>
           <p className={classes.join( ' ' )}>en React!</p>
-          <button
-            style={style}
-            onClick={this.togglePersonsHandler}>Clickea!</button>
+          <StyledButton
+            onClick={this.togglePersonsHandler}>
+            Clickea!
+          </StyledButton>
           {persons}
         </div>
     );
